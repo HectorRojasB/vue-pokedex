7d8fd585ab659d206c2a8ef83ddd9adb14fd707e
Hector Rojas Barcenas <hectorrojasbarcenas@MacBook-Air-de-Hector.local>
2022-12-27 13:42:01 -0600

    Include item card styles and markup

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       <div class="cell large-8 main-container">
         <Header />
         <Search />
+        <div class="items-container">
+          <PokemonCard
+            name="Aron"
+            number="#304"
+            image-url="https://assets.pokemon.com/assets/cms2/img/pokedex/full/304.png"
+            type="steel"
+          ></PokemonCard>
+        </div>
       </div>
     </div>
   </div>
@@ -11,4 +19,5 @@
 <script setup lang="ts">
 import Header from "@/components/Header.vue";
 import Search from "@/components/Search.vue";
+import PokemonCard from "@/components/PokemonCard.vue";
 </script>
--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -1,3 +1,25 @@
 <template>
-  <div class="pokemon-card">dsf</div>
+  <div class="pokemon-card" :class="type">
+    <div class="number-container">
+      <p class="body-text small number">{{ number }}</p>
+    </div>
+    <div class="image-container">
+      <img class="image" :src="imageUrl" :alt="name" />
+    </div>
+    <div class="name-container">
+      <p class="body-text small">{{ name }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    number: String,
+    imageUrl: String,
+    name: String,
+    type: String,
+  },
+};
+</script>
